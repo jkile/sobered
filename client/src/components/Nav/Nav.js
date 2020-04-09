7360c37d8f803c4a9400ec35bc69eca2a30c721a
git stash <git@stash>
2020-04-09 13:45:14 -0700

    index on Dane: 509a4be Merge pull request #2 from jkile/jake

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {nav} from "../Nav/Nav.module.scss"
+
+
+function Nav() {
+  return (
+    <div className={nav}><p>sobered.</p><div className="flexEnd"><p>Sign in</p>
+        </div>
+    </div>
+  );
+}
+
+export default Nav;
