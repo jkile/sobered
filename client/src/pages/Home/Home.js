f1aa1ce5c8b5a2e1aa70191077b8433e184500be
Jake Kile <jkile9@gmail.com>
2020-04-10 01:08:23 -0700

    added base layout for home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,49 +5,48 @@ import Tags from "../../components/Tags/Tags";
 import Nav from "../../components/Nav/Nav";
 import SearchBar from "../../components/SearchBar/SearchBar"
 import GroupCard from "../../components/GroupCard/GroupCard";
+import styles from "./Home.module.scss";
 
 export default function Home() {
     const [value, setValue] = useState(false);
 
     const groups = [
-      {
-        groupName: "HA Tempe",
-        days: ["Mon", "Wed", "Sat"],
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, id totam! Est repellendus voluptate doloribus! Ea maxime quos eligendi praesentium.",
-        tags:["#HA"]
-      },
-      {
-        groupName: "AA Phoenix",
-        days: ["Mon", "Tues", "Fri"],
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, id totam! Est repellendus voluptate doloribus! Ea maxime quos eligendi praesentium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, repellat.",
-        tags:["#AA", "#JF"]
-      },
-      {
-        groupName: "Some Name",
-        days: ["Tues"],
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, id totam! Est repellendus voluptate doloribus! Ea maxime quos eligendi praesentium.",
-        tags:["#AA", "#HA", "#BO", "#LA", "#FJ"]
-      }
-      ];
-  
+        {
+            groupName: "HA Tempe",
+            days: ["Mon", "Wed", "Sat"],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, id totam! Est repellendus voluptate doloribus! Ea maxime quos eligendi praesentium.",
+            tags: ["#HA"]
+        },
+        {
+            groupName: "AA Phoenix",
+            days: ["Mon", "Tues", "Fri"],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, id totam! Est repellendus voluptate doloribus! Ea maxime quos eligendi praesentium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, repellat.",
+            tags: ["#AA", "#JF"]
+        },
+        {
+            groupName: "Some Name",
+            days: ["Tues"],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, id totam! Est repellendus voluptate doloribus! Ea maxime quos eligendi praesentium.",
+            tags: ["#AA", "#HA", "#BO", "#LA", "#FJ"]
+        }
+    ];
+
     return (
-      <div>
-        <Nav />
-        <Button />
-        <Switch
-          isOn={value}
-          onColor="#0ce5e1"
-          handleToggle={() => setValue(!value)}
-        />
-        <Button buttonText="Button" />
-        <Tags />
-        <SearchBar />
-        <div className="cardContainer">
-          {groups.map(item => {
-            return <GroupCard {...item} />
-          })}
+        <div>
+            <Nav />
+            <div className={styles.container}>
+                <Switch
+                    isOn={value}
+                    onColor="#0ce5e1"
+                    handleToggle={() => setValue(!value)}
+                />
+                <SearchBar />
+                <div className={styles.cardContainer}>
+                    {groups.map(item => {
+                        return <GroupCard {...item} />
+                    })}
+                </div>
+            </div>
         </div>
-  
-      </div>
     );
 }
