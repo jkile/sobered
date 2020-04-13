ee9438845d7e9aee905772ec2495d109db1e840b
Jake Kile <jkile9@gmail.com>
2020-04-13 02:30:27 -0700

    fixed positioning bug in group modal and added conditionally rendered button

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -4,7 +4,7 @@ import styles from "./Button.module.scss";
 function Button(props){
 
     return(
-        <button className={styles.btn} onClick={props.onClick}>{props.buttonText}</button>
+        <button className={`${styles.btn} ${props.variant ==="accent" ? styles.btnAccent : styles.btnPrimary}`} onClick={props.onClick}>{props.buttonText}</button>
     )
 }
 
