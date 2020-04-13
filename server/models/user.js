7311cf7d00bdc9e5dd099a2de3202aa8ef6ec3ff
Dane Burns <dane@lizaburnsandassociates.com>
2020-04-13 11:59:40 -0700

    he was a router boi

---
+++ server/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  groups: Array,
+  username: String,
+  password: String,
+  ownedGroups: Array,
+  avatar: String
+
+});
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email: email })
+
+  if (!user) {
+      throw new Error('Unable to login')
+  }
+  
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) {
+      throw new Error('Unable to login')
+  }
+
+  return user
+}
+
+// hashing plaintext password before saving to DB
+userSchema.pre('save', async function (next) {
+  const user = this
+
+  if (user.isModified('password')) {
+      user.password = await bcrypt.hash(user.password, 8)
+  }
+
+  next()
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
